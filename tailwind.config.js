9dc45cc84c6cb7e44a8b73182e5c41042443131e
KakuElvis <kakuelvis@gmail.com>
2022-06-30 22:23:32 +0000

    ecommerce

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}","*.html","./pages/**/*.html","./features/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#CCCC33',
+        'second-main': '#6666FF',
+        'text': '#1F2937  ',        
+      },
+    },
+  },
+  plugins: [],
+}
